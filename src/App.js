c326cd78e4894291e63135793d0a37ccf65336de
Monishasaravanan17 <monisha.saravanan@softsuave.com>
2021-09-21 00:54:14 -0700

    commit Hookscountertwo

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
-import HookCounter from './component/HooksCounter';
-import HooksCounterone from './component/HooksCounterone';
+// import HookCounter from './component/HooksCounter';
+// import HooksCounterone from './component/HooksCounterone';
+import HooksCountertwo from './component/Hookscountertwo';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <h1>Hello world</h1>
-      <HookCounter/>
-      <HooksCounterone/>
+       {/* <HookCounter/> */}
+       {/* <HooksCounterone/> */}
+      <HooksCountertwo/>
     </div>
   );
 }
