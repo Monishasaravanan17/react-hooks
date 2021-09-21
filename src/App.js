4d69fb41d74d7e455f8f66d4e8a67d4065ebc3be
Monishasaravanan17 <monisha.saravanan@softsuave.com>
2021-09-20 23:46:16 -0700

    commit Hookscounter

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import logo from './logo.svg';
+import HookCounter from './component/HooksCounter';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <h1>Hello world</h1>
+      <HookCounter/>
     </div>
   );
 }
